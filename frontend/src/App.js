9c82d8f7cc7f9513937a59c82fb4aaa13e9a7c30
Sumit <sumitscience2017@gmail.com>
2023-07-13 22:15:20 +0530

    frontend part is added

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MainForm from "./components/form"
+
+
+function App() {
+  return (
+    <div className="App">
+      <MainForm />
+    </div>
+  );
+}
+
+export default App;
