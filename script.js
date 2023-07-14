06010e2d34c67a70bb850d550e359ad2feed35fe
Sanskruti Kolgane <96910867+Sansgithub22@users.noreply.github.com>
2023-07-14 08:46:49 +0530

    Task Updated!!!

--- script.js
+++ script.js
@@ -1,66 +1,63 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyD-1YUBnqzCmeSveTC_qEqZRYlWYeQUS5E",
-  authDomain: "regform-46ab8.firebaseapp.com",
-  databaseURL: "https://regform-46ab8-default-rtdb.firebaseio.com",
-  projectId: "regform-46ab8",
-  storageBucket: "regform-46ab8.appspot.com",
-  messagingSenderId: "125959196911",
-  appId: "1:125959196911:web:75162fabdf45f988a94b1a"
-};
+    apiKey: "AIzaSyA0hA2Bc74PEzeuoahNApERIEkxFqv_jSo",
+    authDomain: "techteamexpansion23.firebaseapp.com",
+    projectId: "techteamexpansion23",
+    storageBucket: "techteamexpansion23.appspot.com",
+    messagingSenderId: "542718392377",
+    appId: "1:542718392377:web:bf6178e1b28494fe8c35e8",
+    measurementId: "G-39B1R2EWV2"
+  };
+
 
 firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
-var contactFormDB = firebase.database().ref("regForm");
-
-document.getElementById("contactForm").addEventListener("submit", submitForm);
+const collectionName = 'SANSKRUTI_CSE';
+document.getElementById("registrationForm").addEventListener("submit", submitForm);
 function validateInputs(name, email, branch, phone) {
-
-  var nameRegex = /^[a-zA-Z]+$/;
-  if (!nameRegex.test(name)) {
-      alert("Wrong Name Input | Registration Failed");
-      return false;
-  }
-  var emailRegex = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$/;
-  if (!emailRegex.test(email)) {
-      alert("Wrong Email Input | Registration Failed");
-      return false;
-  }
-  var phoneRegex = /^\d{10}$/;
-  if (!phoneRegex.test(phone)) {
-      alert("Wrong Phone Input | Registration Failed");
-      return false;
-  }
-  return true;
+    // Validate name
+    var nameRegex = /^[a-zA-Z]+$/;
+    if (!nameRegex.test(name)) {
+        alert("Wrong Name Input | Registration Failed");
+        return false;
+    }
+    var emailRegex = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Wrong Email Input | Registration Failed");
+        return false;
+    }
+    var phoneRegex = /^\d{10}$/;
+    if (!phoneRegex.test(phone)) {
+        alert("Wrong Phone Input | Registration Failed");
+        return false;
+    }
+    return true;
 }
 function submitForm(e) {
-  e.preventDefault();
+    e.preventDefault();
 
-  var name = getElementVal("name");
-  var emailid = getElementVal("emailid");
-  var branch = getElementVal("branch");
-  var phone = getElementVal("phone");
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var branch = getElementVal("branch");
+    var phone = getElementVal("phone");
+    const formData = {
+        name: name,
+        email: emailid,
+        branch: branch,
+        phone: phone
+    };
 
-  var check = validateInputs(name, emailid, branch, phone);
-  if (check) {
-      saveMessages(name, emailid, branch, phone);
-      alert("Registration Successful");
-  }
+    var check = validateInputs(name, emailid, branch, phone);
+    if (check) {
 
-  document.getElementById("contactForm").reset();
+
+        db.collection(collectionName).add(formData);
+        alert("Registration Successful");
+    }
+
+    document.getElementById("registrationForm").reset();
 }
 
-const saveMessages = (name, emailid, branch, phone) => {
-  var newContactForm = contactFormDB.push();
-
-  newContactForm.set({
-      name: name,
-      emailid: emailid,
-      branch: branch,
-      phone: phone,
-  });
-};
-
 const getElementVal = (id) => {
-  return document.getElementById(id).value;
+    return document.getElementById(id).value;
 };
-
